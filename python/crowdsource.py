0012b3287e1417d593ebbaeb305e3ba3922e9396
andrew-saydjari <saydjari@g.harvard.edu>
2021-06-15 21:24:26 -0400

    pqdm.processes

--- python/crowdsource.py
+++ python/crowdsource.py
@@ -16,7 +16,7 @@ entirely simultaneously.
 
 import numpy
 import pdb
-import pqdm
+from pqdm.processes import pqdm
 import psf as psfmod
 import scipy.ndimage.filters as filters
 from collections import OrderedDict
