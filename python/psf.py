f69ac6a2b10ccde4207f552587f9d2f4660f4af5
andrew-saydjari <saydjari@g.harvard.edu>
2021-06-16 21:09:52 -0400

    set_title

--- python/psf.py
+++ python/psf.py
@@ -693,13 +693,13 @@ def plot_psf_fits(stamp, x, y, model, isig, name=None):
     fig = p.figure(figsize=(24,8), dpi=150)
     ax = fig.add_subplot(1,3,1)
     ax.imshow(datim, aspect='equal', vmin=-0.005, vmax=0.005, cmap='cet_bkr')
-    p.title('Stamps')
+    ax.set_title('Stamps')
     ax = fig.add_subplot(1,3,2)
     ax.imshow(modim, aspect='equal', vmin=-0.005, vmax=0.005, cmap='cet_bkr')
-    ax.title('Model')
+    ax.set_title('Model')
     ax = fig.add_subplot(1,3,3)
     ax.imshow(datim-modim, aspect='equal', vmin=-0.001, vmax=0.001, cmap='cet_bkr')
-    ax.title('Residuals')
+    ax.set_title('Residuals')
     p.savefig('psf_'+str(name[1])+'_'+name[0]+'.png', dpi=150, bbox_inches='tight', pad_inches=0.1)
 
 
