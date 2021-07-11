a3d0a78b46b1410a24129d7dec527a971dfdba43
andrew-saydjari <saydjari@g.harvard.edu>
2021-07-11 01:16:41 -0400

    histogram saving code with inplace operations

--- python/decam_proc.py
+++ python/decam_proc.py
@@ -288,7 +288,12 @@ def process_image(imfn, ivarfn, dqfn, outfn=None, overwrite=False,
             modhdulist.append(fits.CompImageHDU(modelim, hdr, **compkw))
             hdr['EXTNAME'] = hdr['EXTNAME'][:-4] + '_SKY'
             modhdulist.append(fits.CompImageHDU(skyim, hdr, **compkw))
+            if msk is not None:
+                hdr['EXTNAME'] = hdr['EXTNAME'][:-4] + '_MSK'
+                modhdulist.append(fits.CompImageHDU(msk.astype('i4'), hdr, **compkw))
             if contmask == True:
+                ## here we grossly reuse the arrays previously allocated
+                ## in order to save memory and allocation times
                 scale=8
                 from ternary.helpers import simplex_iterator
                 d = []
@@ -298,21 +303,23 @@ def process_image(imfn, ivarfn, dqfn, outfn=None, overwrite=False,
                     dkey.append((i,j))
                 darr = np.array(d)
 
-                tempdata = np.array([prb[:,:,0].flatten(),prb[:,:,1].flatten(),(prb[:,:,2]+prb[:,:,3]).flatten()])
-                outv = np.empty((len(d),tempdata.shape[1]))
-                np.sum((darr[:,:,np.newaxis]-tempdata[np.newaxis,:,:])**2,axis=1,out=outv)
-                argminlist = np.argmin(outv,axis=0)
+                prb[:,:,2] += prb[:,:,3]
+                prb[:,:,3] = 3
+                np.copyto(mskcnt,-1)
+                for i in range(len(d)):
+                    np.sum((tempdata-darr[i,:,np.newaxis,np.newaxis])**2,axis=0,out=decnum)
+                    np.less(decnum,prb[:,:,3],out=msk)
+                    prb[:,:,3][msk] = decnum[msk]
+                    np.copyto(mskcnt,i,where=msk)
+
                 cnts = np.zeros(len(d))
                 for i in range(len(d)):
-                    cnts[i] = np.sum(np.equal(argminlist,i))
-                cnts *= len(d)/tempdata.shape[1]
+                    cnts[i] = np.sum(np.equal(mskcnt,i))
+                cnts *= len(d)/(prb.shape[0]*prb.shape[1])
                 c1 = fits.Column(name='NLRE_keys0', array=np.array(dkey)[:,0], format='I')
                 c2 = fits.Column(name='NLRE_keys1', array=np.array(dkey)[:,1], format='I')
                 c3 = fits.Column(name='NLRE_vals', array=cnts, format='E')
                 modhdulist.append(fits.BinTableHDU.from_columns([c1, c2, c3],name='NLREDict'))
-            if msk is not None:
-                hdr['EXTNAME'] = hdr['EXTNAME'][:-4] + '_MSK'
-                modhdulist.append(fits.CompImageHDU(msk.astype('i4'), hdr, **compkw))
             modhdulist.close(closed=True)
         count += 1
         if count > nproc:
