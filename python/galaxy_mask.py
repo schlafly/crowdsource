f6cfac1a1e166be85fe93cc81235175e87c00b02
andrew-saydjari <saydjari@g.harvard.edu>
2021-07-16 13:39:51 -0400

    stray colon

--- python/galaxy_mask.py
+++ python/galaxy_mask.py
@@ -81,7 +81,7 @@ def galaxy_mask(header,leda):
 
         print(np.clip(ixn-mhn,a_min=0,a_max=None))
         print(np.clip(ixn+mhn+1,a_min=None,a_max=sz[0]-1))
-        print(np.clip(mhn-ixn,a_min=0,a_max=None):)
+        print(np.clip(mhn-ixn,a_min=0,a_max=None))
         print(np.clip(sz[0]-ixn+mhn,a_min=None,a_max=mszn-1))
         outmsk[np.clip(ixn-mhn,a_min=0,a_max=None):np.clip(ixn+mhn+1,a_min=None,a_max=sz[0]-1),
            np.clip(iyn-mhn,a_min=0,a_max=None):np.clip(iyn+mhn+1,a_min=None,a_max=sz[1]-1)] |= smsk[
