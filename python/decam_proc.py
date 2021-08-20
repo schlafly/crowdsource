6040eba2f0e447d1ab1998760e9b70ef9ff3772f
andrew-saydjari <saydjari@g.harvard.edu>
2021-08-20 18:46:01 -0400

    printing to diagonse process pool errors

--- python/decam_proc.py
+++ python/decam_proc.py
@@ -367,9 +367,10 @@ def process_image(base, date, filtf, vers, outfn=None, overwrite=False,
     for res in iterator:
         if nthreads > 1:
             try:
+                print(res.result())
                 res = res.result()
             except Exception as e:
-                print('Exception running ccd', e)
+                print('Exception running ccd.', e)
                 continue
         if res is None:  # no need to process this extension.
             continue
