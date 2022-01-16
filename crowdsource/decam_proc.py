6be7756f2543d05eb68247c89251b5ab05b41002
andrew-saydjari <saydjari@g.harvard.edu>
2022-01-16 15:24:20 -0500

    try changing decam_proc for once

--- crowdsource/decam_proc.py
+++ crowdsource/decam_proc.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append('/n/home12/saydjari/finksage/ExtSoftware/crowdsource_new/crowdsource')
 import pdb
 import argparse
 import numpy
