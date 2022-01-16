e9c53113e5fafb3f28f2551ea649ea1d99c53ed7
andrew-saydjari <saydjari@g.harvard.edu>
2022-01-16 15:20:19 -0500

    sys append one up

--- crowdsource/crowdsource_base.py
+++ crowdsource/crowdsource_base.py
@@ -15,8 +15,7 @@ entirely simultaneously.
 
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-import crowdsource
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 import numpy
 import pdb
 import crowdsource.psf as psfmod
